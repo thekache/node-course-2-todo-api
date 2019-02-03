ffed88437d4c22877dd4b9a268a03a4bea79bbca
thekache <gio.yache@gmail.com>
2019-02-03 22:29:32 +0400

    Init commit

---
+++ playground/mongodb-find.js
@@ -0,0 +1,18 @@
+const {MongoClient, ObjectID} = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, client) => {
+    if(err){
+        return console.log('Unable to connect to MongoDB server');
+    }
+    console.log('Connencted to MongoDB server');
+    const db = client.db('TodoApp');
+
+    db.collection('Todos').find().toArray().then((docs) => {
+        console.log('Todos');
+        console.log(JSON.stringify(docs, undefined, 2));
+    }, (err) => {
+        console.log('Unable to fetch todos', err)
+    });
+
+    client.close();
+});
